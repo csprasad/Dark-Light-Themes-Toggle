536f23984acbe2d28f5cb9673afeec05aa32e0f4
CSPrasad <csprasad195@gmail.com>
2022-06-10 18:19:52 +0530

    no message

--- Medium - toggle dark light mode/Views/MyTabBarController.swift
+++ Medium - toggle dark light mode/Views/MyTabBarController.swift
@@ -8,23 +8,18 @@
 import UIKit
 
 class MyTabBarController: UITabBarController {
-    
+
     var helper = Helper()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         startObserving(&UserInterfaceStyleManager.shared)
         
         NotificationCenter.default.addObserver(self, selector: #selector(applicationDidBecomeActive), name: UIApplication.didBecomeActiveNotification, object: nil)
-        
     }
     
     @objc func applicationDidBecomeActive() {
-        
         helper.updateThemeState(themeChoice: helper.currentTheme())
-        
-        
     }
     
 }
